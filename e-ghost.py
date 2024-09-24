0ca84245de66a3faa636fd47ba38408233114dfb
~ Sanatani Ghost <166104224+karndeepkumar@users.noreply.github.com>
2024-09-24 17:30:25 +0530

    e-ghost.py
    No System Is Safe 🚬

---
+++ e-ghost.py
@@ -0,0 +1,25 @@
+# *  Author  : Sanatani Ghost 🚬 
+# *  GitHub  : https://github.com/akashblackhat
+# *  YouTub  : SANATANI GHOST 🚬
+# *  Instagram : @sanatani.ghost
+# *  License : MIT
+
+import smtplib, sys, os, random
+from os import system
+
+OKGREEN = '\033[92m'
+WARNING = '\033[0;33m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+LITBU = '\033[94m'
+YELLOW = '\033[3;33m'
+CYAN = '\033[0;36'
+colors = ['\033[92m', '\033[91m', '\033[0;33m']
+RAND = random.choice(colors)
+
+GMAIL_PORT = '587'
+
+
+# AKASH BANER
+def artwork():
+    print("\n")
